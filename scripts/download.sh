b560efafafda77733b1b71af219accf36a5b4e30
Jonas Hein <jonas.hein@inf.ethz.ch>
2025-12-12 15:15:39 +0100

    Added option to download subsets, cleaner helptext formatting, multi-threaded decompression

--- scripts/download.sh
+++ scripts/download.sh
@@ -2,17 +2,19 @@
 
 # Parse command line arguments
 POSITIONAL_ARGS=()
+SUBSETS=()
+
 while [[ $# -gt 0 ]]; do
   case $1 in
     -h|--help)
       echo "Usage: $0 [options] <download_dir>"
       echo "Options:"
-      echo "\t -a \t Short-hand for -d -u -r."
-      echo "\t -d \t Download the dataset."
-      echo "\t -v \t Verify archive integrity."
-      echo "\t -u \t Unpack the dataset."
-      echo "\t -r \t Remove the downloaded archives."
-      echo "\t -h \t Show this help."
+      echo -e " -a \t\t Short-hand for -d -u -r."
+      echo -e " -d [<subsets>]\t Download the dataset. Optionally takes a list of space-separated subset names to download. Valid subset names are train_wetlab train_synth train_pbr test_wetlab test_orx ."
+      echo -e " -v \t\t Verify archive integrity."
+      echo -e " -u \t\t Unpack the dataset."
+      echo -e " -r \t\t Remove the downloaded archives."
+      echo -e " -h \t\t Show this help."
       exit 0
       ;;
     -a)
@@ -24,6 +26,11 @@ while [[ $# -gt 0 ]]; do
     -d)
       DOWNLOAD=true
       shift # past argument
+      # Consume arguments until the next flag or end of args
+      while [[ $# -gt 0 ]] && ! [[ "$1" =~ ^- ]]; do
+        SUBSETS+=("$1")
+        shift
+      done
       ;;
     -v)
       VERIFY=true
@@ -48,15 +55,37 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
-DATASET_DIR="$1"
+
+# Handle edge case: User puts download_dir at the end of the subset list
+# Example: ./download.sh -s sub1 sub2 dir/
+# In this case, 'dir/' is inside SUBSETS and POSITIONAL_ARGS is empty.
+if [[ ${#POSITIONAL_ARGS[@]} -eq 0 ]] && [[ ${#SUBSETS[@]} -gt 0 ]]; then
+    DATASET_DIR="${SUBSETS[-1]}"
+    unset 'SUBSETS[${#SUBSETS[@]}-1]'
+else
+    DATASET_DIR="$1"
+fi
+
+if [ -z "$DATASET_DIR" ]; then
+    echo "Error: No dataset directory specified."
+    echo "Usage: $0 [options] <download_dir>"
+    exit 1
+fi
+
 EXEC_DIR=$(pwd)
 
 echo "MVPSP Dataset Downloader"
-echo "Download: ${DOWNLOAD-false}"
-echo "Verify: ${VERIFY-false}"
-echo "Unpack: ${UNPACK-false}"
-echo "Remove archives: ${REMOVE_ARCHIVES-false}"
-echo "Dataset location: ${DATASET_DIR}"
+if [[ ${#SUBSETS[@]} -gt 0 ]]; then
+  echo -e "Selected Subsets: \t${SUBSETS[*]}"
+else
+  echo -e "Selected Subsets: \tAll"
+fi
+echo -e "Download: \t\t${DOWNLOAD-false}"
+echo -e "Verify: \t\t${VERIFY-false}"
+echo -e "Unpack: \t\t${UNPACK-false}"
+echo -e "Remove archives: \t${REMOVE_ARCHIVES-false}"
+echo -e "Dataset location: \t${DATASET_DIR}"
+echo ""
 
 mkdir -p "${DATASET_DIR}" && cd "${DATASET_DIR}"
 retVal=$?
@@ -77,7 +106,20 @@ if [[ -v DOWNLOAD ]]; then
     exit 1;
   fi
   echo "Downloading MVPSP dataset to ${DATASET_DIR}..."
-  ${AZCOPY} copy --recursive --overwrite=false 'https://rocs2.blob.core.windows.net/mvpsp-public-mirror/*?sv=2022-11-02&ss=bfqt&srt=sco&sp=rlx&se=2029-07-17T17:39:08Z&st=2024-07-17T09:39:08Z&spr=https&sig=cITpUGxTo1aNHvFaMysP%2BCS%2Fy9OHfvXiwuoXGTSlBC4%3D' .
+  AZCOPY_ARGS="copy --recursive --overwrite=false"
+
+  # Handle Subsets via include-pattern
+  if [[ ${#SUBSETS[@]} -gt 0 ]]; then
+    # Always include the small base archive
+    INCLUDE_PATTERNS="mvpsp.tar.xz"
+    for subset in "${SUBSETS[@]}"; do
+      INCLUDE_PATTERNS="${INCLUDE_PATTERNS};${subset}*.tar.xz"
+    done
+    AZCOPY_ARGS="${AZCOPY_ARGS} --include-pattern \"${INCLUDE_PATTERNS}\""
+  fi
+
+  echo "${AZCOPY} ${AZCOPY_ARGS} 'https://rocs2.blob.core.windows.net/mvpsp-public-mirror/*?sv=2022-11-02&ss=bfqt&srt=sco&sp=rlx&se=2029-07-17T17:39:08Z&st=2024-07-17T09:39:08Z&spr=https&sig=cITpUGxTo1aNHvFaMysP%2BCS%2Fy9OHfvXiwuoXGTSlBC4%3D' ."
+  eval "${AZCOPY} ${AZCOPY_ARGS} 'https://rocs2.blob.core.windows.net/mvpsp-public-mirror/*?sv=2022-11-02&ss=bfqt&srt=sco&sp=rlx&se=2029-07-17T17:39:08Z&st=2024-07-17T09:39:08Z&spr=https&sig=cITpUGxTo1aNHvFaMysP%2BCS%2Fy9OHfvXiwuoXGTSlBC4%3D' ."
   retVal=$?
   if [ ${retVal} -ne 0 ]; then
     echo "Download error: azcopy returned ${retVal}"
@@ -105,7 +147,7 @@ fi
 # unpack dataset
 if [[ -v UNPACK ]]; then
   echo "Unpacking dataset..."
-  COMMAND="echo {} ; tar -xJf {}"
+  COMMAND="echo {} ; XZ_OPT='-T0' tar -xJf {}"
   if [[ -v REMOVE_ARCHIVES ]]; then
     COMMAND="${COMMAND} && rm {}"
   fi
